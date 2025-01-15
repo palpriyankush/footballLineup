3970ecc365e1d111fc6585b9be2371e902f4ba23
Priyankush <>
2025-01-15 22:17:14 +0530

    got the starting line up

--- createLineup.js
+++ createLineup.js
@@ -1,5 +1,20 @@
 import lineups from './lineups.json' with {type: 'json'};
 
-const filteredLineup = lineups.lineup.filter((player) => player.positions.some((position) => position.from === "00:00"));
+export const getStarters = function () {
+  const startingEleven = this
+    .filter((player) => player.positions
+      .some((position) => position.from === "00:00"))
+    .map((player) => player.player_name);
 
-console.log(filteredLineup);
+  return startingEleven.join('\n');
+};
+
+const getLineupDetails = (lineups) => {
+  const lineup = lineups.lineup;
+  const getStarting11 = getStarters.bind(lineup);
+
+  console.log(getStarting11());
+
+};
+
+getLineupDetails(lineups);
--- lineup_test.js
+++ lineup_test.js
@@ -1,2 +1,21 @@
 import { describe, test } from "jsr:@std/testing/bdd";
 import { assertEquals } from "jsr:@std/assert";
+import { getStarters } from "./createLineup.js";
+import lineups from './lineups.json' with {type: 'json'};
+
+Deno.test('return the starting line up', () => {
+  const sampleData = lineups.lineup;
+  const expectedOutput = `Sergio Busquets i Burgos
+Jordi Alba Ramos
+Gerard Piqué Bernabéu
+Luis Alberto Suárez Díaz
+Ivan Rakitić
+Ousmane Dembélé
+Samuel Yves Umtiti
+Lionel Andrés Messi Cuccittini
+Nélson Cabral Semedo
+Sergi Roberto Carnicer
+Marc-André ter Stegen`;
+
+  assertEquals(getStarters.call(sampleData), expectedOutput);
+});
